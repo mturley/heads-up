074bdd9faf12bf4af5e8741d813d53fe40513a0c
Mike Turley <mike@miketurley.com>
2013-02-12 23:59:45 -0500

    implementing dummy config flag "favorite color" to test pipelines

--- heads-up.js
+++ heads-up.js
@@ -3,8 +3,34 @@ if (Meteor.isClient) {
     return Meteor.user() !== null;
   };
   Template.body.configured = function() {
-    return false;
-  }
+    var conf;
+    return (conf=db.UserConfigs.findOne({ userId : Meteor.userId() })) !== undefined && conf.confirmed;
+  };
+  Template.config.events({
+    'click #config-submit' : function() {
+      var config = {
+        userId    : Meteor.userId(),
+        foo       : 'bar',
+        favcolor  : $("#config-favcolor").val(),
+        confirmed : true 
+      };
+      // figure out if we already have a saved config
+      if(db.UserConfigs.find({ userId : config.userId }).count() === 0) {
+        db.UserConfigs.insert(config);
+      } else {
+        db.UserConfigs.update({ userId : config.userId }, { $set: config });
+      }
+    }
+  });
+  Template.dashboard.events({
+    'click #config' : function(e) {
+      e.preventDefault();
+      db.UserConfigs.update({ userId: Meteor.userId() }, { $set: { confirmed : false }});
+    }
+  });
+  Template.dashboard.favcolor = function() {
+    return db.UserConfigs.findOne({ userId : Meteor.userId() }).favcolor;
+  };
 }
 
 if (Meteor.isServer) {
@@ -12,3 +38,9 @@ if (Meteor.isServer) {
     // code to run on server at startup
   });
 }
+
+
+// Collections
+db = {
+  UserConfigs : new Meteor.Collection("UserConfigs")
+};
